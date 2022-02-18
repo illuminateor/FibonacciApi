6387ccbe125331c60da98dec8f969e89e4c6db7f
illuminateor <o.mudrak93@gmail.com>
2022-02-18 15:56:36 +0100

    Add project files.

---
+++ Controllers/FibonacciController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FibonacciApi.Entities;
+
+namespace FibonacciApi.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class FibonacciController : ControllerBase
+    {
+        [HttpGet("{number}")]
+        public ActionResult<FibonacciNumber> CheckNumber(int number)
+        {
+            FibonacciNumber num = new FibonacciNumber();
+            num.Number = number;
+
+            return Ok(num);
+        }
+    }
+}
---
+++ FibonacciNumber.cs
@@ -0,0 +1,51 @@
+﻿namespace FibonacciApi.Entities
+{
+    public partial class FibonacciNumber
+    {
+        public int Number { get; set; }
+        public bool IsFibonnaci => CheckIsFibonacci(Number);
+
+        public bool IsPrime => CheckIsPrime(Number);
+
+        private bool CheckIsFibonacci(int number)
+        {
+            if (number == 0) return true;
+
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < 52; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+
+                if (a == number)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        private bool CheckIsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+}
